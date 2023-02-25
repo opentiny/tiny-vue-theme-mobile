c648410aec85ec7895352bfabbe1ef57720f3944
TC-twwang <741682830@qq.com>
2023-02-25 15:45:50 +0800

    feat(multi-select): 增加下拉选择器multi-select组件

---
+++ src/wheel/index.js
@@ -0,0 +1,13 @@
+export default {
+  "tiny-mobile-wheel-content-font-size": "14px",
+  "tiny-mobile-wheel-wrapper-height": "224px",
+  "tiny-mobile-wheel-wrapper-font-size": "14px",
+  "tiny-mobile-wheel-wrapper-scroll-margin-top": "38px",
+  "tiny-mobile-wheel-scroll-hasFooter-lineHeight": "36px",
+  "tiny-mobile-wheel-scroll-hasFooter-height": "36px",
+  "tiny-mobile-wheel-scroll-hasFooter-color": "#595959",
+  "tiny-mobile-wheel-scroll-hasFooter-lineHeight": "44px",
+  "tiny-mobile-wheel-scroll-hasFooter-height": "44px",
+  "tiny-mobile-wheel-scroll-hasFooter-color": "#191919",
+  "tiny-mobile-wheel-scroll-icon-font-size": "20px",
+};
