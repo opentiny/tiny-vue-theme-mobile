d8b758dd7d4f3c354b89943ab5859884a7c34da1
TC-twwang <741682830@qq.com>
2023-03-03 17:39:34 +0800

    feat(form): form组件增加自动换行

--- src/action-sheet/index.js
+++ src/action-sheet/index.js
@@ -1,11 +1,11 @@
 export default {
-  'tiny-mobile-action-sheet-content-bgcolor': '#ddd',
-  'tiny-mobile-action-sheet-menu-bgcolor': '#fff',
-  'tiny-mobile-action-sheet-item-color': '#333',
-  'tiny-mobile-action-sheet-item-warn-color': '#f36f64',
-  'tiny-mobile-action-sheet-item-font-size': '16px',
-  'tiny-mobile-action-sheet-item-border-color': '#ddd',
-  'tiny-mobile-action-sheet-item-active-bgcolor': '#ececec',
-  'tiny-mobile-action-sheet-item-active-color': '#039be5',
-  'tiny-mobile-action-sheet-action-bgcolor': '#fff'
-}
+  "tiny-mobile-action-sheet-content-bg-color": "#fff",
+  "tiny-mobile-action-sheet-menu-bg-color": "#fff",
+  "tiny-mobile-action-sheet-item-text-color": "#333",
+  "tiny-mobile-action-sheet-item-warn-text-color": "#f36f64",
+  "tiny-mobile-action-sheet-item-font-size": "12px",
+  "tiny-mobile-action-sheet-item-border-color": "#ddd",
+  "tiny-mobile-action-sheet-item-active-bg-color": "#ececec",
+  "tiny-mobile-action-sheet-item-active-text-color": "#039be5",
+  "tiny-mobile-action-sheet-action-bg-color": "#fff",
+};
