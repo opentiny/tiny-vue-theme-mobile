293a1ebc40c17d62453dc8a10060451813047a02
James-9696 <hujm969696@163.com>
2026-08-17 15:34:29 +0800

    fix: 解决打包报错问题：找不到opentiny/vue-theme文件

--- packages/theme-mobile/build/gulp-dist.js
+++ packages/theme-mobile/build/gulp-dist.js
@@ -8,26 +8,72 @@ const cssmin = require('gulp-clean-css')
 const prefixer = require('gulp-autoprefixer')
 const fg = require('fast-glob')
 const fs = require('node:fs')
+const path = require('path')
 
 const source = '../src'
 const dist = '../dist'
+
+function resolveVueTheme(importPath) {
+  const candidates = [
+    '@opentiny/vue-theme/' + importPath,
+    '@opentiny/vue-theme/' + importPath.replace(/\.css$/, '.less')
+  ]
+  for (const c of candidates) {
+    try {
+      return require.resolve(c, {
+        paths: [
+          path.resolve(__dirname, '../'),
+          path.resolve(__dirname, '../../')
+        ]
+      })
+    } catch (e) {
+      throw e
+    }
+  }
+  return null
+}
 
 // 将所有组件下的index.less合并到src下的index.less
 const fileList = fg.sync('../src/*/index.less')
 const importStr = fileList
   .map((filePath) => filePath.replace('../src/', './'))
-  .map((path) => `@import '${path}';`)
+  .map((p) => `@import '${p}';`)
   .join('\n')
 const note = fs.readFileSync('../src/index.less', { encoding: 'utf-8' }).match(/(^\/\*\*.+?\*\/)/s)[0]
 fs.writeFileSync('../src/index.less', `${note}\n\n${importStr}`)
 
+// Less PreProcessor：在 less 解析每个文件前，把 @opentiny/vue-theme/xxx 替换为绝对路径
+const VueThemeResolver = {
+  install: function(less, pluginManager) {
+    pluginManager.addPreProcessor({
+      process: function(src, extra) {
+        return src.replace(
+          /@import\s+["']@opentiny\/vue-theme\/([^"']+)["']/g,
+          function(match, importPath) {
+            const resolved = resolveVueTheme(importPath)
+            if (resolved) {
+              return '@import "' + resolved.replace(/\\/g, '/') + '"'
+            }
+            console.warn('Warning: Cannot resolve @opentiny/vue-theme/' + importPath)
+            return match
+          }
+        )
+      }
+    })
+  }
+}
+
 gulp.task('compile', () => {
   return gulp
     .src([`${source}/**/index.less`, `${source}/index.less`])
-    .pipe(less())
+    .pipe(
+      less({
+        plugins: [VueThemeResolver]
+      })
+    )
     .pipe(
       prefixer({
-        borwsers: ['last 1 version', '> 1%', 'not ie <= 8'],
+        overrideBrowserslist: ['last 1 version', '> 1%', 'not ie <= 8'],
         cascade: true,
         remove: true
       })
